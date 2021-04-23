c65b6bdb02510b200de24e17818c5a33be81043c
Vitor Gonçalves <a10848@alunos.ipca.pt>
2021-04-23 18:21:22 +0100

    sensor pir muda estado dos leds

--- Trabalho_Pratico_SETR/Trabalho_Pratico_SETR.ino
+++ Trabalho_Pratico_SETR/Trabalho_Pratico_SETR.ino
@@ -1,170 +1,64 @@
-/*
- Name:		SETR_ALARM_V2.ino
- Created:	22/04/2021 18:54:02
- Author:	vitor
-*/
-
-#include <Arduino.h>
-
-/*Bibliotecas FreeRTOS */
 #include <Arduino_FreeRTOS.h>
 
-/*mapeamento de pinos*/
-#define PIR1 2
-#define PIR2 3
-// #define MAG1 5
-// #define MAG2 5
-// #define MAG3 6
-#define LED_ON 12
-#define LED_OFF 13
+// task
+void Task_Led(void* param);
+void Task_Pir(void* param);
 
-/* Variáveis para armazenamento do handle das tasks*/
-TaskHandle_t taskPir1Handle = NULL;
-TaskHandle_t taskPir2Handle = NULL;
-// TaskHandle_t taskMag1Handle = NULL;
-// TaskHandle_t taskMag2Handle = NULL;
-// TaskHandle_t taskMag3Handle = NULL;
-// TaskHandle_t taskLedOnHandle = NULL;
-// TaskHandle_t taskLedOffHandle = NULL;
+// handle
+TaskHandle_t Task_Led_Handle;
+TaskHandle_t Task_Pir_Handle;
 
-/*protítipos das Tasks*/
-void vTaskPir1(void* pvParameters);
-void vTaskPir2(void* pvParameters);
-// void vTaskMag1(void* pvParameters);
-// void vTaskMag2(void* pvParameters);
-// void vTaskMag3(void* pvParameters);
-// void vTaskLedOn(void* pvParameters);
-// void vTaskLedOff(void* pvParameters);
+// pin
+#define led1 12
+#define led2 13
+#define pir1 2
+#define pir2 3
 
-/*variáveis auxiliares*/
-bool alarmArmed = false;
-int valueSensorTicksTime = 1000;
+// variable
+bool pirSensorActive = false;
 
-// the setup function runs once when you press reset or power the board
 void setup()
 {
 	Serial.begin(9600);
 
-	xTaskCreate(vTaskPir1, "TASK_PIR_1", configMINIMAL_STACK_SIZE, (void*)PIR1, 1, &taskPir1Handle);
-	xTaskCreate(vTaskPir2, "TASK_PIR_2", configMINIMAL_STACK_SIZE, (void*)PIR2, 1, &taskPir2Handle);
-	// xTaskCreate(vTaskMag1, "TASK_MAG_1", configMINIMAL_STACK_SIZE, (void*)MAG1, 1, &taskMag1Handle);
-	// xTaskCreate(vTaskMag2, "TASK_MAG_2", configMINIMAL_STACK_SIZE, (void*)MAG2, 1, &taskMag2Handle);
-	// xTaskCreate(vTaskMag3, "TASK_MAG_3", configMINIMAL_STACK_SIZE, (void*)MAG3, 1, &taskMag3Handle);
-	// xTaskCreate(vTaskLedOn, "TASK_LED_ON", configMINIMAL_STACK_SIZE, (void*)LED_ON, 1, &taskLedOnHandle);
-	// xTaskCreate(vTaskLedOff, "TASK_LED_OFF", configMINIMAL_STACK_SIZE, (void*)LED_OFF, 1, &taskLedOffHandle);
+	pinMode(led1, OUTPUT);
+	pinMode(led2, OUTPUT);
+	pinMode(pir1, INPUT);
+	pinMode(pir2, INPUT);
+
+	// task create
+	xTaskCreate(Task_Led, "TASK_LED", 100, NULL, 1, &Task_Led_Handle);
+	xTaskCreate(Task_Pir, "TASK_PIR", 100, NULL, 1, &Task_Pir_Handle);
 }
 
-// the loop function runs over and over again until power down or reset
-void loop()
-{
-	if (alarmArmed == true)
-	{
-		digitalWrite(LED_ON, HIGH);
-		digitalWrite(LED_OFF, LOW);
-	}
-	else
-	{
-		digitalWrite(LED_ON, LOW);
-		digitalWrite(LED_OFF, HIGH);
-	}
-}
+void loop() {}
 
-/// <summary>
-/// le o valor do sensor PIR 1
-/// </summary>
-/// <param name="pvParameters"></param>
-void vTaskPir1(void* pvParameters)
-{
-	pinMode(PIR1, INPUT);
+void Task_Led(void* param) {
+	(void)param;
 
-	while (1)
-	{
-		int value = digitalRead(PIR1);
-
-		if (value == HIGH)
-		{
-			alarmArmed = true;
-			Serial.println("PIR_1 Reading..." + (String)value);
+	while (1) {
+		if (pirSensorActive == true) {
+			digitalWrite(led1, HIGH);
+			digitalWrite(led2, LOW);
 		}
-		else
-		{
-			alarmArmed = false;
+		else {
+			digitalWrite(led1, LOW);
+			digitalWrite(led2, HIGH);
 		}
-		pdMS_TO_TICKS(valueSensorTicksTime);
+		vTaskDelay(10 / portTICK_PERIOD_MS);
 	}
 }
 
-/// <summary>
-/// le o valor do sensor PIR 2
-/// </summary>
-/// <param name="pvParameters"></param>
-void vTaskPir2(void* pvParameters)
-{
-	pinMode(PIR2, INPUT);
+void Task_Pir(void* param) {
+	(void)param;
 
-	while (1)
-	{
-		int value = digitalRead(PIR2);
-
-		if (value == HIGH)
-		{
-			alarmArmed = true;
-			Serial.println("PIR_2 Reading...");
+	while (1) {
+		if (digitalRead(pir1) == HIGH || digitalRead(pir2) == HIGH) {
+			pirSensorActive = true;
 		}
-		else
-		{
-			alarmArmed = false;
+		else {
+			pirSensorActive = false;
 		}
-		pdMS_TO_TICKS(valueSensorTicksTime);
+		vTaskDelay(10 / portTICK_PERIOD_MS);
 	}
 }
-
-/*void vTaskMag2(void* pvParameters)
-{
-	pinMode(MAG1, INPUT);
-
-	while (1)
-	{
-		int value = digitalRead(MAG1);
-
-		Serial.println("MAG_1: " + (String)value);
-
-		pdMS_TO_TICKS(valueSensorTicksTime);
-	}
-}
-
-void vTaskLedOn(void* pvParameters)
-{
-	pinMode(LED_ON, OUTPUT);
-
-	while (1)
-	{
-		if (alarmArmed == true)
-		{
-			digitalWrite(LED_ON, HIGH);
-		}
-		else
-		{
-			digitalWrite(LED_ON, LOW);
-		}
-		pdMS_TO_TICKS(valueSensorTicksTime);
-	}
-}
-
-void vTaskLedOff(void* pvParameters)
-{
-	pinMode(LED_OFF, OUTPUT);
-
-	while (1)
-	{
-		if (alarmArmed == false)
-		{
-			digitalWrite(LED_OFF, HIGH);
-		}
-		else
-		{
-			digitalWrite(LED_OFF, LOW);
-		}
-		pdMS_TO_TICKS(valueSensorTicksTime);
-	}
-}*/
